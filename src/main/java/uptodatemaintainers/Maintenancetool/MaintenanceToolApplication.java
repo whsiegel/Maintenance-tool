673869ec285e43fbac9d4e44c61637e917eaa55b
William Siegel <whsiegel@gmail.com>
2019-06-18 15:44:37 -0500

    initial commit

---
+++ src/main/java/uptodatemaintainers/Maintenancetool/MaintenanceToolApplication.java
@@ -0,0 +1,12 @@
+package uptodatemaintainers.Maintenancetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceToolApplication {
+
+	public static void main(String[] args) { SpringApplication.run(MaintenanceToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uptodatemaintainers/Maintenancetool/MaintenanceToolApplicationTests.java
@@ -0,0 +1,16 @@
+package uptodatemaintainers.Maintenancetool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MaintenanceToolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
