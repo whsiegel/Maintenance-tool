32d85927ff210471da50bcf0f7ed1684fead6c7d
William Siegel <whsiegel@gmail.com>
2019-07-02 12:03:58 -0500

    work order add page and controller linking, need to handle posts

--- src/main/java/uptodatemaintainers/Maintenancetool/controllers/WorkOrderController.java
+++ src/main/java/uptodatemaintainers/Maintenancetool/controllers/WorkOrderController.java
@@ -1,10 +1,12 @@
 package uptodatemaintainers.Maintenancetool.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import uptodatemaintainers.Maintenancetool.models.Data.AssetDao;
 import uptodatemaintainers.Maintenancetool.models.Data.WorkOrderDao;
 import uptodatemaintainers.Maintenancetool.models.WorkOrder;
 
@@ -13,14 +15,19 @@ import uptodatemaintainers.Maintenancetool.models.WorkOrder;
 public class WorkOrderController {
 
     @Autowired
+    AssetDao assetDao;
+
+    @Autowired
     WorkOrderDao workOrderDao;
 
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @GetMapping("/add")
     public String createWorkOrder(Model model){
         model.addAttribute("title", "Add Work Order");
         model.addAttribute(new WorkOrder());
+        model.addAttribute("assets", assetDao.findAll());
 
-        return "/add";
+        return "WorkOrder/add";
     }
 
 
